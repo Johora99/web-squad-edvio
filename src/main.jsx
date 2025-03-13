035c8d32452ed8ac8e8356d790eaf48e8cc5e308
Ismail <ismailhosen8757@gmail.com>
2025-03-14 00:54:21 +0600

    Login And Register

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import './index.css'
 import RouterProvider from "./RouterProvider/RouterProvider";
+import AuthProvider from "./AuthProvider/AuthProvider";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
+  <AuthProvider>
   <RouterProvider></RouterProvider>
+  </AuthProvider>
   </BrowserRouter>
 );
 
