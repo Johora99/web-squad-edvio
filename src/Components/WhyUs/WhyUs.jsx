ae71a195894585926620ddd203a55c895e4e2945
Tanzim52 <161871173+Tanzim52@users.noreply.github.com>
2025-03-15 06:39:08 +0600

    added a bit styling for the heading

--- src/Components/WhyUs/WhyUs.jsx
+++ src/Components/WhyUs/WhyUs.jsx
@@ -6,7 +6,7 @@ const WhyUs = () => {
     return (
         <div className=' p-4 mx-auto pt-32 pb-64 bg-LightTeal '>
             <div className=' flex flex-col items-center mb-10'>
-                <h2 className='text-start text-8xl text-gray-200 font-extrabold'>Why Choose <span>EdVio?</span></h2>
+                <h2 className='text-start text-8xl text-gray-200 font-extrabold'>Why Choose <span className=' bg-gradient-to-r from-teal-600 to-indigo-500 bg-clip-text text-transparent'>EdVio?</span></h2>
             </div>
             <div className=''>
                 {features.map((feature, index) => (
