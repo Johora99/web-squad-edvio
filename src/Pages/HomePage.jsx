c60efff3a6d60c21fea030503b734f9cb7dd0630
Johora99 <fatimatuzjohora1999@gmail.com>
2025-03-15 20:53:04 +0600

    whyUs update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,17 +5,22 @@ import ContactUs from "../components/ContactUs";
 import Reviews from "../components/Reviews";
 import AllCourses from "../Components/AllCourses/AllCourses";
 import Pro from "./PremiumCourse/Pro";
+import FreeCourses from "../Components/FreeCourses/FreeCourses";
+import WhyUs from "../Components/WhyUs/WhyUs";
 
 export default function HomePage() {
   return (
     <>
       <Banner></Banner>
-
       <div className="container w-11/12 mx-auto">
         <section className="my-10 lg:my-20">
           <AllCourses></AllCourses>
         </section>
+        <section>
+         <FreeCourses></FreeCourses>
+        </section>
         <Pro />
+        <WhyUs></WhyUs>
         <CommonQuestion></CommonQuestion>
         <Reviews></Reviews>
         <ContactUs></ContactUs>
